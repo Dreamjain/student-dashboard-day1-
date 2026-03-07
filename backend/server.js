ba33dcf86388da9e91a470fb8c9aa65684bb7a69
DreamJain10 <dreampachori18@gmail.com>
2026-03-07 21:58:29 +0530

    project day 2

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,41 @@
+const Student = require("./models/studentModel");
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db.js");
 
 const app = express();
+console.log("SERVER FILE LOADED");
 
-connectDB(); // connects backend to MongoDB
+connectDB();
 
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Student Dashboard Backend Running 🚀");
+});
+
+app.get("/students", async (req, res) => {
+  console.log("GET /students route hit");
+
+  try {
+    const students = await Student.find();
+    res.json(students);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.post("/students", async (req, res) => {
+  try {
+    const student = new Student(req.body);
+
+    const savedStudent = await student.save();
+
+    res.status(201).json(savedStudent);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 const PORT = 5000;
