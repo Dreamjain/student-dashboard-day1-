ea80fb8876ae1fd8dfa40f2bcbc5c46ae44f2535
DreamJain10 <dreampachori18@gmail.com>
2026-04-01 22:25:35 +0530

    project day 18

--- backend/controllers/studentController.js
+++ backend/controllers/studentController.js
@@ -151,6 +151,22 @@ const getAllStudents = async (req, res) => {
   }
 };
 
+exports.loginStudent = async (req, res) => {
+  try {
+    const { rollNumber } = req.body;
+
+    const student = await Student.findOne({ rollNumber });
+
+    if (!student) {
+      return res.status(404).json({ message: "Student not found" });
+    }
+
+    res.json(student);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
 module.exports = {
   createStudent,
   getStudents,
