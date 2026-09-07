784e5ac63a1a3850e7bc394dd8e60bfde1240ebe
Dreamjain <dp2292@srmist.edu.in>
2026-09-08 02:02:49 +0530

    feat(auth): secure faculty password storage

--- backend/models/facultyModel.js
+++ backend/models/facultyModel.js
@@ -1,9 +1,48 @@
 const mongoose = require("mongoose");
+const { hashPassword, isPasswordHash } = require("../utils/password");
 
-const facultySchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  password: String
+const facultySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 2,
+      maxlength: 100
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      match: /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+      select: false
+    }
+  },
+  { timestamps: true }
+);
+
+facultySchema.pre("save", async function hashFacultyPassword(next) {
+  if (!this.isModified("password") || isPasswordHash(this.password)) {
+    return next();
+  }
+
+  this.password = await hashPassword(this.password);
+  return next();
+});
+
+facultySchema.set("toJSON", {
+  transform: (_doc, ret) => {
+    delete ret.password;
+    delete ret.__v;
+    return ret;
+  }
 });
 
 module.exports = mongoose.model("Faculty", facultySchema);
